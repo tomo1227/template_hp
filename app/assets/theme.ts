c7277883b066fd68dcc3e9e146f5bd7186e16957
tomo1227 <tomo.soccer1227@gmail.com>
2024-10-29 08:10:26 +0000

    ソースコードのハイライトを修正

--- app/assets/theme.ts
+++ app/assets/theme.ts
@@ -1,18 +1,16 @@
 const theme = () => {
-  const currentTheme = localStorage.getItem("theme");
   const isDark = window.matchMedia("(prefers-color-scheme: dark)").matches;
-  if (currentTheme === null) {
+  console.log("localStorage.theme: ", localStorage.theme)
+
+  if (!('theme' in localStorage) || localStorage.theme === 'system' ) {
     if (isDark) {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
+      document.documentElement.classList.add('dark')
     }
+    localStorage.setItem('theme', 'system')
+  } else if (localStorage.theme === 'dark') {
+    document.documentElement.classList.add('dark')
   } else {
-    if (currentTheme === "dark") {
-      document.documentElement.classList.add("dark");
-    } else {
-      document.documentElement.classList.remove("dark");
-    }
+    document.documentElement.classList.remove('dark')
   }
 };
 
