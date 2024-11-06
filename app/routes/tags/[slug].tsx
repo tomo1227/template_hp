58dd4d05b8cfa6ddfe6cf193539ea918dd56c9a3
tomo1227 <tomo.soccer1227@gmail.com>
2024-11-06 14:17:25 +0000

    一度簡単な例で試す

--- app/routes/tags/[slug].tsx
+++ app/routes/tags/[slug].tsx
@@ -25,31 +25,35 @@ export default createRoute(
     }
 
     return c.render(
-      <div>
-        <div class={"flex flex-col mb-10 items-center"}>
-          <h1
-            class={`text-center leading-tight text-3xl mb-0 mt-6 pb-2 font-bold flex justify-center items-center space-x-2 flex-wrap`}
-          >
-            <TitleIcon iconUrl="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Pushpin/Flat/pushpin_flat.svg" size={30} />
-            &nbsp;{tag}
-          </h1>
-        </div>
-        <div class={"mt-6 flex flex-col gap-12"}>
-          {posts.map((post) => (
-            <Fragment key={post.entryName}>
-              <ArticleListItem
-                title={post.frontmatter.title}
-                createdDate={post.frontmatter.createdDate}
-                updatedDate={post.frontmatter.updatedDate}
-                description={post.frontmatter.description}
-                iconUrl={post.frontmatter.iconUrl}
-                tags={post.frontmatter.tags}
-                entryName={post.entryName}
-              />
-            </Fragment>
-          ))}
-        </div>
-      </div>
-    );
+      <h1>{tag}</h1>
+    )
+
+    // return c.render(
+    //   <div>
+    //     <div class={"flex flex-col mb-10 items-center"}>
+    //       <h1
+    //         class={`text-center leading-tight text-3xl mb-0 mt-6 pb-2 font-bold flex justify-center items-center space-x-2 flex-wrap`}
+    //       >
+    //         <TitleIcon iconUrl="https://raw.githubusercontent.com/microsoft/fluentui-emoji/main/assets/Pushpin/Flat/pushpin_flat.svg" size={30} />
+    //         &nbsp;{tag}
+    //       </h1>
+    //     </div>
+    //     <div class={"mt-6 flex flex-col gap-12"}>
+    //       {posts.map((post) => (
+    //         <Fragment key={post.entryName}>
+    //           <ArticleListItem
+    //             title={post.frontmatter.title}
+    //             createdDate={post.frontmatter.createdDate}
+    //             updatedDate={post.frontmatter.updatedDate}
+    //             description={post.frontmatter.description}
+    //             iconUrl={post.frontmatter.iconUrl}
+    //             tags={post.frontmatter.tags}
+    //             entryName={post.entryName}
+    //           />
+    //         </Fragment>
+    //       ))}
+    //     </div>
+    //   </div>
+    // );
   }
 );
