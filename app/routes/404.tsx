be2153b411ee869526bbfe9622788311a5f360f9
tomo1227 <tomo.soccer1227@gmail.com>
2024-11-10 11:50:42 +0000

    404の編集

--- app/routes/404.tsx
+++ app/routes/404.tsx
@@ -2,9 +2,9 @@ import { createRoute } from "honox/factory";
 
 export default createRoute((c) => {
   return c.render(
-    <div class="mt-6 flex flex-col justify-center items-center h-32">
+    <div class="mt-6 flex flex-col justify-center items-center mb-96">
       <h1 class="text-6xl py-6">404 Not Found</h1>
-      <h2 class="text-3xl">お探しのページが見つかりませんでした。</h2>
+      <h2 class="text-2xl">お探しのページが見つかりませんでした。</h2>
     </div>
   );
 }
