2b03829a1308e8b6f74fc65a8dbdcfcf81624de3
tomo1227 <tomo.soccer1227@gmail.com>
2024-11-10 11:58:44 +0000

    footerにmt-autoを追加

--- app/routes/404.tsx
+++ app/routes/404.tsx
@@ -2,7 +2,7 @@ import { createRoute } from "honox/factory";
 
 export default createRoute((c) => {
   return c.render(
-    <div class="mt-6 flex flex-col justify-center items-center mb-96">
+    <div class="mt-6 flex flex-col justify-center items-center">
       <h1 class="text-6xl py-6">404 Not Found</h1>
       <h2 class="text-2xl">お探しのページが見つかりませんでした。</h2>
     </div>
