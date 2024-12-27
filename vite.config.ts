7ce3fac67f2f3cd30efa1bdf7f9f9cb18bfcc4e8
tomo1227 <tomo.soccer1227@gmail.com>
2024-12-27 21:15:12 +0000

    不要なのを削除

--- vite.config.ts
+++ vite.config.ts
@@ -32,7 +32,7 @@ export default defineConfig(({ mode }): UserConfig => {
       light: "everforest-light",
     },
     defaultLang: "plaintext",
-    transformers: [transformerNotationDiff(), transformerNotationHighlight()],
+    transformers: [transformerNotationDiff()],
   };
 
   const commonConfig = {
